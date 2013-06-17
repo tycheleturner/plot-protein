60cea72f26006d8c0de440ce4c21bdb21acd148a
tycheleturner <tycheleturner@gmail.com>
2013-06-17 16:50:29 -0400

    Update ui.R as part of Shiny app

--- ui.R
+++ ui.R
@@ -1,6 +1,16 @@
+#!/bin/R
+#Tychele N. Turner
+#Laboratory of Aravinda Chakravarti, Ph.D.
+#Johns Hopkins University School of Medicine
+#Protein Plotting Script for Shiny
+#Programming Language: R
+#Updated 06/15/2013
+
+#Description: This script is the ui.R script for the Plot Protein Shiny Application
+
 library(shiny)
 
-# Define UI for random distribution application 
+# Define UI 
 shinyUI(pageWithSidebar(
 
   # Application title
@@ -20,6 +30,8 @@ shinyUI(pageWithSidebar(
 	textInput("proteinLength", "Length of Protein:", "Data"),
 	
 	textInput("nameOfQuery", "Name of Query:", "Data"),
+	
+	textInput("tickSize", "Tick Size:", 10),	
 
 	checkboxInput(inputId = "labels",
       label = strong("Show Labels"),
@@ -35,8 +47,7 @@ shinyUI(pageWithSidebar(
 		 
   ),
 
-  # Show a tabset that includes a plot, summary, and table view
-  # of the generated distribution
+  # Show a tabset that includes a plot and table
   mainPanel(
 	h5("Tychele N. Turner, tycheleturner@gmail.com"),
 	h5("Laboratory of Aravinda Chakravarti, Ph.D."),
